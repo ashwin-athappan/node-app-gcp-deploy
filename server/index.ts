47cf6a734f1c291b6f1de2c0ec37415600440a72
ashwin <ashwinathappank@gmail.com>
2023-05-21 23:51:20 +0530

    fixes

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (req: Request, res: Response) => {
+app.get('/api/users/get', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
